f141094cc2abce00c07a0d957c1549066da69378
fahraday <ghosterinc@gmail.com>
2022-03-22 20:09:33 -0400

    feat: adding option to chose what contact group we want

--- ContactsScript.ts
+++ ContactsScript.ts
@@ -1,17 +1,15 @@
-const CONTACT_GROUP_ID = 'http://www.google.com/m8/feeds/groups/kevinjosemotab%40gmail.com/base/358067f78e7fe312'
-
-
 /**
  * It search for an specific group of contacs and returns the primary email address of all the contacts whithin that group
+ * @param groupName - the name of a contact group
  * @returns  an array of email addresses of all the contacts in the specified group
  */
-const getContacts = (): string[]  => {
-  const contactGroup = ContactsApp.getContactGroupById(CONTACT_GROUP_ID)
+const getContacts = (groupName: string): string[] => {
+  const contactGroup = ContactsApp.getContactGroup(groupName)
   const contactsRaw = contactGroup.getContacts();
   const contacts: string[] = []
   //for each contact store its primary email address in the {contacts} variable
   contactsRaw.forEach(contact => {
-    contacts.push(contact.getEmailAddresses()[0])
+    contacts.push(contact.getAddresses()[0].getAddress())
   })
   return contacts;
 }
