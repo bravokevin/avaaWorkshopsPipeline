f7b0b2af3f64b8f5214b872ce0f09a0332be6feb
fahraday <ghosterinc@gmail.com>
2022-03-27 22:28:36 -0400

    feat: set mechanism to avoid resend the last workshop

--- SpreadsheetUI.ts
+++ SpreadsheetUI.ts
@@ -4,7 +4,12 @@
  * @module 
  */
 
+/**
+ * The column from where we get the Group name we want to send the workshops
+ */
 const RANGE_FOR_GROUP_NAME = 'E7';
+
+const LAST_WORKSHOP_PROPWETY_KEY = "last_workshop_property";
 
 const onOpen = () => {
   const ui = SpreadsheetApp.getUi();
@@ -25,12 +30,26 @@ const sendWorkshop = () => {
     })
   }
   catch (e) {
-    ui.alert(e)
+    ui.alert(
+      'Please confirm',
+      'Are you sure you want to continue?',
+      ui.ButtonSet.YES_NO);
+
     return
   }
   if (ContactsApp.getContactGroup(groupName) === null) {
-    ui.alert("Este grupo de contactos no existe. Ingresa un grupo de contacto existente.");
+    ui.alert('Ocurrio un problema!', 'Este grupo de contactos no existe. Ingresa un grupo de contacto existente.', ui.ButtonSet.OK);
     return;
+  }
+  const lastWorkshopStored = scriptProperties.getProperty(LAST_WORKSHOP_PROPWETY_KEY)
+
+  if (values.length === 1 && values[0][0].toString() === lastWorkshopStored) {
+    const result = ui.alert(
+      `El taller "${values[0][1]}" ya fue enviado`,
+      '¿Deseas enviarlo de nuevo?',
+      ui.ButtonSet.YES_NO);
+    // Process the user's response.
+    if (result == ui.Button.NO) return
   }
   const prompt = ui.prompt('Coloca el asunto del correo')
   const promptResponse = prompt.getResponseText()
@@ -42,6 +61,7 @@ const sendWorkshop = () => {
     ui.alert(e)
     return
   }
+  scriptProperties.setProperty(LAST_WORKSHOP_PROPWETY_KEY, values[0][0].toString())
   //Once finished inform throught the UI that the process was completed succesfully
   SpreadsheetApp.getUi()
     .alert(`Los talleres: \n ${talleresEnviados.toString().replaceAll(',', '\n')} \n han sido enviados exitosamente!!`);
